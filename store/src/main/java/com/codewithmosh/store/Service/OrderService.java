832142dc9e960bd3ad0a325fdba3e7bcf4024bc2
73mtq <147471544+73mtq@users.noreply.github.com>
2025-07-12 22:40:37 +0800

    2025.7.12
    Service；Autowired，Qualifier

--- store/src/main/java/com/codewithmosh/store/Service/OrderService.java
+++ store/src/main/java/com/codewithmosh/store/Service/OrderService.java
@@ -1,7 +1,18 @@
 package com.codewithmosh.store.Service;
 
-public class OrderService {
-    public void placeOrder() {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 
+@Service
+public class OrderService {
+    private PaymentService paymentService;
+    @Autowired
+    public OrderService(@Qualifier("paypal") PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    public void placeOrder() {
+        paymentService.processPayment(100);
     }
 }
--- store/src/main/java/com/codewithmosh/store/StoreApplication.java
+++ store/src/main/java/com/codewithmosh/store/StoreApplication.java
@@ -1,13 +1,19 @@
 package com.codewithmosh.store;
 
+import com.codewithmosh.store.Service.OrderService;
+import com.codewithmosh.store.Service.PayPalPaymentService;
+import com.codewithmosh.store.Service.StripePaymentService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class StoreApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(StoreApplication.class, args);
+     ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+        var orderService = context.getBean(OrderService.class);
+        orderService.placeOrder();
     }
 
 }
